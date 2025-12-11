d40dbe8f87789883a19be5220192a79990529e06
kiartz <41741259+kiartz@users.noreply.github.com>
2025-12-11 14:13:17 +0100

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/CuePack-v0.1/', // Percorso specifico per la tua repository GitHub
+  base: './', // Use relative paths for flexible deployment on GitHub Pages
 })
